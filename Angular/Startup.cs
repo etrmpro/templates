ee1b58cc221bec320e89eb8292c791aecc7b49da
Stephen Nimmo <stephennimmo@gmail.com>
2015-11-18 08:09:24 -0600

    init

---
+++ Angular/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(CurveManagement.Startup))]
+
+namespace CurveManagement
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
---
+++ Angular/app/app.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app', ['ngRoute']);
+
+    app.config(['$routeProvider',
+      function ($routeProvider) {
+          $routeProvider.
+            when('/index.html', {
+                templateUrl: 'app/index/index.html',
+                controller: 'indexController',
+                controllerAs: 'controller'
+            }).
+            otherwise({
+                redirectTo: '/index.html'
+            });
+      }]);
+
+})();
