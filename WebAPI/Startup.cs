eef60759ed76f491e060a8f4fb7a3f31aa8e5fdd
Stephen Nimmo <stephennimmo@gmail.com>
2015-11-18 09:02:48 -0600

    webapi working

---
+++ WebAPI/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using System.Net.Http.Headers;
+
+[assembly: OwinStartup(typeof(WebAPI.Startup))]
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            app.UseWebApi(config);
+        }
+    }
+}
